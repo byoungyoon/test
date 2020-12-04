8f7dd69c49df5d1b79299bee51be3d2bc4dd6c97
byoungyoon <bur5698@navercom>
2020-12-04 16:28:23 +0900

    [이병윤] 초반 기초 작업

--- lms/src/main/java/gd/fintech/lms/admincontroller/LoginAdminController.java
+++ lms/src/main/java/gd/fintech/lms/admincontroller/LoginAdminController.java
@@ -2,11 +2,17 @@ package gd.fintech.lms.admincontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import gd.fintech.lms.adminservice.LoginAdminService;
 
 @Controller
 public class LoginAdminController {
 	@Autowired LoginAdminService loginAdminService;
+	
+	@GetMapping(value = {"/", "/adminLogin"})
+	public String index() {
+		return "admin/login";
+	}
 
 }
--- lms/src/main/java/gd/fintech/lms/studentcontroller/LoginStudentController.java
+++ lms/src/main/java/gd/fintech/lms/studentcontroller/LoginStudentController.java
@@ -2,10 +2,16 @@ package gd.fintech.lms.studentcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import gd.fintech.lms.studentservice.LoginStudentService;
 
 @Controller
 public class LoginStudentController {
 	@Autowired LoginStudentService loginStudentService;
+	
+	@GetMapping("/studentLogin")
+	public String index() {
+		return "student/login";
+	}
 }
